ccde02c6169103caf6e07d8f92ef26025fe5368d
CodeManBist <sagarbist372@gmail.com>
2026-03-14 21:21:02 +0530

    feat(init): implement init command to cretae .myGit repository folder

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,23 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+
 async function initRepo() {
-  console.log("Repository initialized");
+  const repoPath = path.resolve(process.cwd(), ".myGit");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    await fs.mkdir(repoPath, { recursive: true });
+    await fs.mkdir(commitsPath, { recursive: true });
+    await fs.writeFile(
+      path.join(repoPath, "config.json"),
+      JSON.stringify({ bucket: "s3 bucket" })
+    );
+    
+    console.log("Repository initialised!");
+  } catch (err) {
+    console.log("Error initialising repository", err);
+  }
 }
 
 module.exports = { initRepo };
