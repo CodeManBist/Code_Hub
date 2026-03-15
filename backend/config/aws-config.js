b9a61ffa58d64ff4ab062b05ed05a2edeef0c13c
CodeManBist <sagarbist372@gmail.com>
2026-03-15 19:17:13 +0530

    changed server from ap-south-1 to eu-north-1

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -1,7 +1,7 @@
 const { S3Client } = require("@aws-sdk/client-s3");
 
 const s3 = new S3Client({
-  region: "ap-south-1"
+  region: "eu-north-1"
 });
 
 const S3_BUCKET = "code-hub-storage-493499579453-eu-north-1-an";
